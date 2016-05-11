e53f380d98176c48d8f93ac0b538048d8842d877
105442VIJI <viji.vodeyar@gmail.com>
2016-05-11 23:43:23 +0530

    Initial stage

---
+++ app/src/main/java/com/example/acadgild/assignment63/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.acadgild.assignment63;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView Hello;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        Hello = (TextView)findViewById(R.id.Hello);
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_red) {
+
+            Hello.setTextColor(getColor(R.color.Red));
+            return true;
+        }
+
+        if (id == R.id.action_green) {
+
+            Hello.setTextColor(getColor(R.color.green));
+            return true;
+        }
+
+        if (id == R.id.action_Blue) {
+
+            Hello.setTextColor(getColor(R.color.Blue));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
